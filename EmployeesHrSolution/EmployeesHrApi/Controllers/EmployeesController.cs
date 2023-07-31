c9172b9be3f7aff1b7a0085260a4fb3b43fae9d5
Amber Mathur <amber_mathur@hotmail.com>
2023-07-31 15:41:48 -0400

    Add connection string

--- EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
+++ EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeesHrApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeesHrApi.Controllers;
 
 public class EmployeesController :ControllerBase
 {
+    private readonly EmployeeDataContext _context;
+
+    public EmployeesController(EmployeeDataContext context)
+    {
+        _context = context;
+    }
+
     [HttpGet("/employees")]
     public async Task<ActionResult> GetEmployeesAsync()
     {
-        return Ok("Tacos are good, I might have some for lunch");
+        var employees = await _context.Employees.ToListAsync();
+        return Ok(employees);
     }
 
 }
--- EmployeesHrSolution/EmployeesHrApi/Program.cs
+++ EmployeesHrSolution/EmployeesHrApi/Program.cs
@@ -1,14 +1,24 @@
-      
-            var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+using EmployeesHrApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+var employeesConnectionString = builder.Configuration.GetConnectionString("employees") ?? throw new Exception("Need a Connection String");
+
+builder.Services.AddDbContext<EmployeeDataContext>(options =>
+{
+    options.UseSqlServer(employeesConnectionString);
+});
+
+var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
